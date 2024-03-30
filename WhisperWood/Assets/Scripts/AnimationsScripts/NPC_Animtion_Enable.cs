1736096ef9cf71ff27a182452c15094f8c614237
Jetre99 <Folk104@canton.edu>
2024-03-30 13:49:39 -0400

    Animations Script outline compete, and a quick hotel fix.
    Hello, everyone. I forgot to add the staircase rail in the hotel scene, and I added more checks for the NPC animation. Now, I just need to know what needs to be linked to the script.

--- WhisperWood/Assets/Scripts/AnimationsScripts/NPC_Animtion_Enable.cs
+++ WhisperWood/Assets/Scripts/AnimationsScripts/NPC_Animtion_Enable.cs
@@ -11,21 +11,52 @@ public class NPC_Animtion_Enable : MonoBehaviour
     {
         NPCAnimationEnable();
         TomNPCAnimation = GameObject.FindWithTag("TomAnimations").GetComponent<Animator>();
+        CoralNPCAnimation = GameObject.FindWithTag("CoralAnimations").GetComponent <Animator>();
+        ElmoreNPCAnimation = GameObject.FindWithTag("ElmoreAnimations").GetComponent<Animator>();
     }
 
     void Update()
     {
-        
+        NPCAnimationEnable();
     }
 
     void NPCAnimationEnable ()
     {
         //Tom
-
-
+        if (TomNPCAnimation.GetBool("NPCRun") == false) //I might need to search for a NPC Run Script part.
+        {
+            TomNPCAnimation.SetBool("NPCRun", true);
+        }
+        else if (TomNPCAnimation.GetBool("NPCWalk") == false)
+        {
+            TomNPCAnimation.SetBool("NPCWalk", true);
+        }
+        else
+        {
+            //The fail safe for the idle animation. This script also can be split up for faster code if needed.
+            TomNPCAnimation.SetBool("NPCRun", false);
+            TomNPCAnimation.SetBool("NPCWalk", false);
+        }
 
         //Elmore
+        if (ElmoreNPCAnimation.GetBool("NPCDrinking") == false)
+        {
+            ElmoreNPCAnimation.SetBool("NPCDrinking", true);
+        }
+        else
+        {
+            ElmoreNPCAnimation.SetBool("NPCDrinking", false);
+        }
 
         //Coral
+        if (CoralNPCAnimation.GetBool("NPCSitting") == false)
+        {
+            CoralNPCAnimation.SetBool("NPCSitting", true);
+        }
+        else
+        {
+            CoralNPCAnimation.SetBool("NPCSitting", false);
+        }
+
     }
 }
